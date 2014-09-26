05b611017bf4f916213035465db76a76d8c28235
Francisco Javier Figueroa Alba <franciscofigueroa87@gmail.com>
2014-09-26 19:44:47 -0400

    Solucion tarea 1

---
+++ tareas/soluciontareaI/franciscofigueroa/App.java
@@ -0,0 +1,14 @@
+
+//Corregir errores
+
+
+public class App { // Cambiamos el nombre de la clase "AppDemo" a "App" ya que si la clase es "public" debe tener el mismo nombre del archivo.
+
+
+   public static void main(String[] args){
+
+         System.out.println("App Demo"); // Agregamos ";" al final para especificar que termina la linea de codigo.
+
+   }
+
+}
---
+++ tareas/soluciontareaI/franciscofigueroa/AppConcatenar.java
@@ -0,0 +1,19 @@
+
+
+public class AppConcatenar {
+
+
+    public static void main(String[] args){
+	
+	String nombre = "Bob";
+	String apellido = "Smith";
+        String nombreCompleto = nombre+ " " + apellido; // Creamos una nueva variable para concatenar las varibles nombre y apellido a la cual hemos llamado nombreCompleto.
+	
+        //Escribir logica para imprimir nombre completo en una sola línea, 
+        //ejemplo: nombreCompleto : Bob Smith
+
+	System.out.println("nombreCompleto: "+nombreCompleto); // Hacemos un llamado variable nombreCompleto
+	
+}
+
+}
