1ebbe395032e3d1c82870eb5e1311713d423d711
Felipe De Castro <fdc500@gmail.com>
2014-09-27 09:40:30 -0400

    Solución Tarea 2
    Felipe De Castro 10-3905

---
+++ tareaII/soluciontareaII/felipedecastro/Empleado.java
@@ -0,0 +1,56 @@
+package org.cursofinalgrado.uapa.java.poo.herencia;
+
+import java.util.Date;
+
+public class Empleado extends Persona implements Sueldo {   
+    private double sueldoFijo;
+    private double horasExtras;
+    private double preciosHorasExtras;
+    
+    private int numeroEmpleado;
+
+    public void setNumeroEmpleado(int numeroEmpleado) {
+        this.numeroEmpleado = numeroEmpleado;
+    }
+
+    public int getNumeroEmpleado() {
+        return numeroEmpleado;
+    }
+    
+    public Empleado(String string, String string1, String string2, Date date) {
+        super(string, string1, string2, date);
+    }
+    
+    public Empleado(){
+        
+    }
+
+    @Override
+    public double calcularSueldo() {
+        return (sueldoFijo + (horasExtras * preciosHorasExtras));
+    }
+
+    public double getSueldoFijo() {
+        return sueldoFijo;
+    }
+
+    public void setSueldoFijo(double sueldoFijo) {
+        this.sueldoFijo = sueldoFijo;
+    }
+
+    public double getHorasExtras() {
+        return horasExtras;
+    }
+
+    public void setHorasExtras(double horasExtras) {
+        this.horasExtras = horasExtras;
+    }
+
+    public double getPreciosHorasExtras() {
+        return preciosHorasExtras;
+    }
+
+    public void setPreciosHorasExtras(double preciosHorasExtras) {
+        this.preciosHorasExtras = preciosHorasExtras;
+    }
+}
