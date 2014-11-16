b4422776254d956541b9626bf5785c07368e2aec
ecabrerar <eudris@gmail.com>
2014-11-15 20:24:39 -0400

    Agregando el patron de diseno singleton

--- ejemplos/programacion-web-java/ejemplos/jsp/jsp-jdbc/src/java/org/cursofinalgrado/uapa/java/jsp/modelo/Coneccion.java
+++ ejemplos/programacion-web-java/ejemplos/jsp/jsp-jdbc/src/java/org/cursofinalgrado/uapa/java/jsp/modelo/Coneccion.java
@@ -3,21 +3,39 @@ package org.cursofinalgrado.uapa.java.jsp.modelo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.text.MessageFormat;
+import java.util.logging.Logger;
+/**
+ * 
+ * @author ecabrerar
+ */
 public class Coneccion { 
+
+    private static Coneccion instancia=null;
+    
+    public Coneccion() {
+        setDriver();
+    } 
+    
+    public static Coneccion getInstancia(){
+        if(instancia==null){
+            instancia=new Coneccion();
+        }
+        
+        return instancia;
+    }
        
-    private static void setDriver(){
+    private  void setDriver(){
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver not found");
+        } catch (ClassNotFoundException e) {            
+            Logger.getLogger(getClass().getName()).info(MessageFormat.format("Driver not found {0}", e.getMessage()));
         }
     }
     
-    public static Connection getConeccion(){
+    public Connection getConeccion(){
         
-        setDriver();
         
         //Establish connection to MySQL database
         String connectionURL = "jdbc:mysql://127.0.0.1/tallerjavadb";
@@ -26,7 +44,7 @@ public class Coneccion {
         try {
             connection = DriverManager.getConnection(connectionURL, "root", "rootweb");
         } catch (SQLException e) {
-            System.out.println("Connection failed !!!");
+            Logger.getLogger(getClass().getName()).info(String.format("Connection failed !!! %s", e.getMessage()));
         }
         
         return connection;
--- ejemplos/programacion-web-java/ejemplos/jsp/jsp-login-form/src/java/org/cursofinalgrado/uapa/java/servlet/modelo/Coneccion.java
+++ ejemplos/programacion-web-java/ejemplos/jsp/jsp-login-form/src/java/org/cursofinalgrado/uapa/java/servlet/modelo/Coneccion.java
@@ -3,35 +3,44 @@ package org.cursofinalgrado.uapa.java.servlet.modelo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * 
+ * @author ecabrerar
+ */
 public class Coneccion {
-    
-    public Coneccion(){
-        setDriver();
+
+    private static final Coneccion INSTANCIA = new Coneccion();
+
+    private Coneccion() {    }
+
+    public static Coneccion getInstancia() {
+        return INSTANCIA;
     }
-    
-    private void setDriver(){
+
+    public Connection getConeccion() {
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            System.out.println("Driver not found");
+            Logger.getLogger(getClass().getName()).info("Driver not found");
+
         }
-    }
-    
-    public static Connection getConeccion(){
-        
+
         //Establish connection to MySQL database
         String connectionURL = "jdbc:mysql://127.0.0.1/tallerjavadb";
-        Connection connection=null;
+        Connection connection = null;
 
         try {
             connection = DriverManager.getConnection(connectionURL, "root", "rootweb");
         } catch (SQLException e) {
-            System.out.println("Connection failed !!!");
+            Logger.getLogger(getClass().getName()).log(Level.INFO, "Connection failed !!!{0}", e.getMessage());
+
         }
-        
+
         return connection;
     }
-    
+
 }
